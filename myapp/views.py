712934b60e18346952f980e17197893b61223176
Uddin Nahim <nahim.211902019@gmail.com>
2025-11-10 03:14:08 +0600

    adding chart js

--- myapp/views.py
+++ myapp/views.py
@@ -3,20 +3,18 @@ from .models import Food ,Consume
 
 # Create your views here.
 def index(request):
+    foods = Food.objects.all()  # always fetch foods
 
     if request.method == "POST":
-        food_consumed = request.POST['food_consumed']
-        consume = Food.objects.get(name=food_consumed)
-        user = request.user
-        consume = Consume(user=user,food_consumed=consume)
-        consume.save()
-        return redirect('index')
-        foods = Food.objects.all()
-        
+        food_consumed_name = request.POST.get('food_consumed')  # safer
+        if food_consumed_name:  # check if user selected a food
+            food_obj = Food.objects.get(name=food_consumed_name)
+            user = request.user
+            consume = Consume(user=user, food_consumed=food_obj)
+            consume.save()
+        return redirect('index')  # redirect after POST
 
-    else:   
-        foods = Food.objects.all()
-    consumed_food =  Consume.objects.filter(user=request.user)
+    consumed_food = Consume.objects.filter(user=request.user)
     return render(request, 'myapp/index.html', {
         "foods": foods,
         "consumed_food": consumed_food
